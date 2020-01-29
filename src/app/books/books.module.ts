1cdaa5208ae10eb623b7ccd7178f6f7afa77e6e0
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-01-29 13:51:30 +0100

    clean-up code

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BooksComponent } from './books.component';
-import { BooksLibraryComponent } from './components/books-library/books-library.component';
+import { NgModule } from '@angular/core';
+
 import { MaterialModule } from '../material/material.module';
 import { BooksRoutingModule } from './books-routing.module';
-import { BookCardComponent } from './components/books-library/book-card/book-card.component';
+import { BooksComponent } from './books.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BooksLibraryComponent } from './components/books-library/books-library.component';
 import { BooksBackendService } from './services/books-backend.service';
 
 @NgModule({
-  declarations: [BooksComponent, BooksLibraryComponent, BookCardComponent, BookDetailsComponent],
+  declarations: [BooksComponent, BooksLibraryComponent, BookDetailsComponent],
   imports: [CommonModule, MaterialModule, BooksRoutingModule],
   providers: [BooksBackendService]
 })
