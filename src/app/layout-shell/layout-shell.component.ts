ad0253fe290014bcd7f1f9784d6a21600c55f297
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-02-05 09:14:06 +0100

    deleted unnecessary compoent | global-nav

--- src/app/layout-shell/layout-shell.component.ts
+++ src/app/layout-shell/layout-shell.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout-shell',
   templateUrl: './layout-shell.component.html',
   styleUrls: ['./layout-shell.component.scss']
 })
-export class LayoutShellComponent implements OnInit {
-  constructor() {}
+export class LayoutShellComponent  {
 
-  ngOnInit() {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+constructor(private breakpointObserver: BreakpointObserver) {}
 }
