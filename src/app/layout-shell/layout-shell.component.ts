392b41f81c3e7c9a72a27dd3c78f72b78f49d5b4
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-02-11 07:18:22 +0100

    added declatations

--- src/app/layout-shell/layout-shell.component.ts
+++ src/app/layout-shell/layout-shell.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthenticationService } from '../user-access/services/authentication.service';
 
 @Component({
   selector: 'app-layout-shell',
   templateUrl: './layout-shell.component.html',
   styleUrls: ['./layout-shell.component.scss']
 })
-export class LayoutShellComponent  {
+export class LayoutShellComponent implements OnInit {
 
+  constructor(private breakpointObserver: BreakpointObserver, private authenticationsService: AuthenticationService) {}
+  public isUser: boolean;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  .pipe(
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(result => result.matches),
     shareReplay()
   );
-
-constructor(private breakpointObserver: BreakpointObserver) {}
+  
+  public ngOnInit(): void {
+    if (this.authenticationsService.currentUserValue !== null) {
+      console.log('zalgowany');
+      this.isUser = true;
+    } else {
+      console.log('nie zalo');
+      this.isUser = false;
+    }
+  }
 }
